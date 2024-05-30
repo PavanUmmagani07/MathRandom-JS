fa4884dacdb09e2254ef055ac086ec59afa5c2da
Pavan Kalyan <127199710+PavanUmmagani07@users.noreply.github.com>
2024-05-30 14:39:08 +0530

    Math.random()

---
+++ Index.js
@@ -0,0 +1,14 @@
+var n = Math.random();
+n = n * 6;
+n = Math.floor(n) + 1;
+console.log(n);
+
+var n = Math.random(1, 6);
+n = Math.floor(n) + 1;
+console.log(n);
+
+prompt("What is your name?");
+prompt("What is their name?");
+var lovescore = Math.random() * 100;
+lovescore = Math.floor(lovescore) + 1
+alert("Your Love Score is"+lovescore+"%");
