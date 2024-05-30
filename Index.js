0d39f8ef67dace98d156b4c0e6740b328bbde86d
Pavan Kalyan <127199710+PavanUmmagani07@users.noreply.github.com>
2024-05-30 14:55:28 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -12,3 +12,29 @@ prompt("What is their name?");
 var lovescore = Math.random() * 100;
 lovescore = Math.floor(lovescore) + 1
 alert("Your Love Score is"+lovescore+"%");
+
+
+prompt("What is your name?");
+prompt("What is their name?");
+var lovescore = Math.random() * 100;
+lovescore = Math.floor(lovescore) + 1
+if(lovescore>70){
+  alert("Your Love Score is"+lovescore+"% "+lovescore +" You love each other like Romeo and Juliet);
+} else{
+  alert("Your Love Score is "+lovescore+"%");
+}
+
+
+var lovescore = Math.random() * 100;
+lovescore = Math.floor(lovescore) + 1;
+console.log(lovescore);
+if (lovescore > 70) {
+  console.log(
+    "Your Love Score is " +
+      lovescore +
+      "% " +
+      "You love each other like Romeo and Juliet",
+  );
+} else {
+  console.log("Your Love Score is " + lovescore + "%");
+}
